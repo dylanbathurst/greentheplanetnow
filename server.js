584cf093f24e7d9c601559396087e023b935c5c9
dylanbathurst <dylanbathurst@gmail.com>
2011-11-19 13:05:34 -0800

    basic node express setup with mu templating

---
+++ server.js
@@ -0,0 +1,34 @@
+var http      = require('http'),
+    mu        = require('mu2'),
+    app       = require('express').createServer(),
+    util      = require('util');
+
+var template = function (res, statusCode, template, view) {
+
+  mu.root = __dirname + '/templates/';
+
+  res.writeHead(statusCode, {
+    'content-type': 'text/html'
+  });
+
+  mu.compile(template + '.html', function (err, parsed) {
+    if (err) {
+      // exports.error(res, 404, 'error compiling template');
+      return;
+    };
+
+    var readableStream = mu.render(template + '.html', view);
+
+    util.pump(readableStream, res, function (err) {
+      // winston.log('error', 'Template Error.', err);
+    });
+  });
+
+};
+
+app.get('/', function (req, res) {
+  template(res, 200, 'layout', {'hello': 'world'});  
+});
+
+app.listen(8000);
+
