c853b52d4e01c3aca184b210dced9685ca052e6e
Rick Duggan <rick.duggan@gmail.com>
2011-11-20 04:57:17 +0000

    Add spaces between category, owner name, etc.

--- public/map.js
+++ public/map.js
@@ -70,11 +70,11 @@ function plotItem(item, map) {
       
     var contentString = '<div><h1><a href="/projects/' + item._id +
                         '">' + item.ProjectName + '</a></h1>' +
-                        '<span>' + item.ProjectCategory + '</span>' +
-                        '<span>' + item.OwnerName + '</span>' +
-                        '<span>' + item.Description + '</span>' +
-                        '<span>' + item.FundingReceived + '</span>' +
-                        '<span>' + item.FundingNeeded + '</span>';
+                        '<span>' + item.ProjectCategory + '<&nbsp></span>' +
+                        '<span>' + item.OwnerName + '<&nbsp></span>' +
+                        '<span>' + item.Description + '<&nbsp></span>' +
+                        '<span>' + item.FundingReceived + '<&nbsp></span>' +
+                        '<span>' + item.FundingNeeded + '<&nbsp></span>';
 
     if (item.ProjectVideo) {
       console.log(item.ProjectVideo.videoId);
